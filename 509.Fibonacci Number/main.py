04a563584f889c49e861659120b1bf775cb0d20c
industriousonesoft <industriousonesoft@gmail.com>
2021-03-16 19:50:45 +0800

    BugFixed

--- 509.Fibonacci Number/main.py
+++ 509.Fibonacci Number/main.py
@@ -1,21 +1,20 @@
-def fib(n):
+def fib(n) -> int:
 	if n < 0:
 		return -1
 	elif n == 0:
 		return 0
 	elif n == 1:
 		return 1
-
 	sum = 0
 	prev = 0
 	curr = 1
-	for i in range(3, n):
+	for i in range(2, n+1):
 		sum = prev + curr
+		print(sum)
 		prev = curr
 		curr = sum
-		pass
 	return sum
 
 if __name__ == '__main__':
-	num = 10
+	num = 2
 	print("fib(", num, ") = ", fib(num))
