09e33b24eb3cbbbb326ef4d6d27ad3f0516c1764
industriousonesoft <industriousonesoft@gmail.com>
2021-03-17 19:41:14 +0800

    53.Maximum Subarray

---
+++ 53.Maximum Subarray/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+int maxSubarray(std::vector<int>& nums) {
+	int max = 0;
+	for (int i = 0; i < nums.size(); ++i)
+	{	int sum_max = nums[i];
+		int sum = nums[i];
+		for (int j = i + 1; j < nums.size(); ++j)
+		{
+			sum += nums[j];
+			sum_max = std::max(sum_max, sum);
+		}
+		max = std::max(max, sum_max);
+	}
+	return max;
+}
+
+int maxSubarray2(std::vector<int>& nums) {
+	std::vector<int> dp_table = std::vector<int>(nums);
+	dp_table[0] = nums[0];
+	int max = 0;
+	for (int i = 1; i < nums.size(); ++i)
+	{
+		dp_table[i] = std::max(nums[i], nums[i] + dp_table[i-1]);
+		max = std::max(max, dp_table[i]);
+	}
+	return max;
+}
+
+int main(int argc, const char * argv[]) {
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+	// std::vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+	std::vector<int> nums = {5,4,-1,7,8};
+
+	std::cout << "maxSubarray: " << maxSubarray2(nums) << std::endl; 
+
+	return 0;
+}
