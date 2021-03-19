0d4b6999e7969886db53b8f9a197d1d76e8ffece
industriousonesoft <industriousonesoft@gmail.com>
2021-03-19 19:24:08 +0800

    Try Binary Seach Solution

--- 300.Longest Increasing Sebsequence/main.cpp
+++ 300.Longest Increasing Sebsequence/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <vector>
 
-//Input: nums = [10,9,2,5,3,7,101,18]
-//Output: 4
-//Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
-// LIS(i) = 1 + LIS(j), j < i, arr[j] << arr[i]
-
+// DP Solution: LIS(i) = 1 + LIS(j), j < i, arr[j] < arr[i]
 int dpLIS(std::vector<int>& nums, int end_index, std::vector<int>& dp_table) {
 	int max_length = 1;
 	for (int i = end_index-1; i >= 0; i--) {
@@ -21,7 +17,7 @@ int dpLIS(std::vector<int>& nums, int end_index, std::vector<int>& dp_table) {
 	return max_length;
 }
  
-int lengthOfLIS(std::vector<int>& nums) {
+int DP_lengthOfLIS(std::vector<int>& nums) {
 	int size = nums.size();
 	if (size == 0) {
 		return 0;
@@ -30,18 +26,22 @@ int lengthOfLIS(std::vector<int>& nums) {
 	}
 	std::vector<int> dp_table(nums.size());
 	int max_length = 0;
-	for (int i = 0; i < nums.size(); ++i)
-	{
+	for (int i = 0; i < nums.size(); ++i) {
 		max_length = std::max(max_length, dpLIS(nums, i, dp_table));
 	}
 	return max_length;
 }
 
+// Dinary Search Soulution:
+int BS_lengthOfLIS(std::vector<int>& nums) {
+	return 0;
+}
+
 int main(int argc, const char * argv[]) {
 	// std::vector<int> nums = {10,9,2,5,3,7,101,18};
-	std::vector<int> nums = {0,1,0,3,2,3};
+	std::vector<int> nums = {6,16,8,7,4,10,20,19,7,25,29,12};
 	// std::vector<int> nums = {7,7,7,7,7,7,7};
-	int ret = lengthOfLIS(nums);
+	int ret = DP_lengthOfLIS(nums);
 	std::cout << "lengthOfLIS: " << ret << std::endl;
 	return 0;
 }
