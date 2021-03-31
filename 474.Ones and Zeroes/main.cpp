cccc7dd3aee22a7ce4b2190ed223b05386a77113
industriousonesoft <industriousonesoft@gmail.com>
2021-03-31 19:37:46 +0800

    Leetcode-474 solution-v1

--- 474.Ones and Zeroes/main.cpp
+++ 474.Ones and Zeroes/main.cpp
@@ -27,26 +27,69 @@ strs[i] consists only of digits '0' and '1'.
 1 <= m, n <= 100
 */
 
+/*
+这道题与“找零钱”问题的逻辑类似，指定用规定的元素来凑齐指定的数目，不同点在于“找零钱”问题中的零钱可以无限次使用，
+而此题的每一个元素只能使用一次。采样动态规划的思想，状态转移方程：dp(k, m, n) = max(dp(k-1, m-x, n-y)+1, dp(k-1, m, n))
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
 
-int countZeroElementOfStr(std::strin& str) {
+int countZeroElementOfStr(std::string& str) {
 	int zero_elem_count = 0;
 	for (int i = 0; i < str.length(); ++i) {
-		if (str[i] == 0) {
+		// std::cout << str[i];
+		if (str[i] == '0') {
 			zero_elem_count += 1;
 		}
 	}
+	// std::cout << std::endl;
 	return zero_elem_count;
+}
+
+// TODO: 取出重叠运算
+int recursive(std::vector<std::string>& strs, int index, int m, int n) {
+	if (index < 0 || index >= strs.size()) {
+		return -1;
+	}
+	std::string cur_elem = strs[index];
+	int zero_count = countZeroElementOfStr(cur_elem);
+	int one_count = cur_elem.length() - zero_count;
+	std::cout << "elem " << cur_elem << " zero_count: " << zero_count << " one_count: " << one_count << std::endl; 
+	std::cout << "elem " << cur_elem << " m: " << m << " n: " << n << std::endl;   
+	if (zero_count == m && one_count == n) {
+		return 1;
+	// 加入当前元素后溢出，则直接进入下一个元素
+	}else if (zero_count > m || one_count > n) {
+		return recursive(strs, index-1, m , n);
+	// 加入当前元素后还存在差值，更新差值进入下一个元素
+	}else {
+		if (index == 0) {
+			return -1;
+		}
+		int sub_len = recursive(strs, index-1, m - zero_count, n - one_count);
+		return  sub_len > 0 ? sub_len + 1 : -1;
+	}
 }
 
 int findMaxForm(std::vector<std::string>& strs, int m, int n) {
 	int arr_size = strs.size();
-	std::string elem = "";
-	return 0;
+	int max_len = 0;
+	for (int i = 0; i < arr_size; ++i) {
+		max_len = std::max(max_len, recursive(strs, i, m, n));
+	}
+	return max_len;
 }
 
-int main(int argc, const char argv[]) {
-	return 0;
+int main(int argc, const char * argv[]) {
+	// std::vector<std::string> strs = {"10","0001","111001","1","0"};
+	// int m = 5;
+	// int n = 3;
+	std::vector<std::string> strs = {"10","0","1"};
+	int m = 1;
+	int n = 1;
+	int max_len = findMaxForm(strs, m, n);
+	std::cout << "find max form: " << max_len << std::endl;
+ 	return 0;
 }
