8ecb31082c7fce68d25f66aa13b3ef45aa68e848
industriousonesoft <industriousonesoft@gmail.com>
2021-04-30 19:35:46 +0800

    Try 64

---
+++ 64.Minimum Path Sum/main.cpp
@@ -0,0 +1,40 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, 
+which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example 1:
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+Example 2:
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minPathSum(vector<vector<int>>& grid) {
+    return 0;
+}
+
+int main(int argc, const char* argv[]) {
+    return 0;
+}
