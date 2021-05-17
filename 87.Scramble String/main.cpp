82ad7cb5daf663cac555a99434ae0c4cdac7e0ef
industriousonesoft <industriousonesoft@gmail.com>
2021-05-17 19:36:58 +0800

    Still working on 87

--- 87.Scramble String/main.cpp
+++ 87.Scramble String/main.cpp
@@ -49,12 +49,13 @@ s1 and s2 consist of lower-case English letters.
 
 using namespace std;
 
+// 时间复杂度O(2^n)
+// 空间复杂度O(n)，map是常量数组不会随着n的变化而变化，空间复杂度来源于递归
 bool isScramble(string s1, string s2) {
 	if (s1.size() != s2.size()) return false;
 	if (s1 == s2) return true;
 	int size = s1.size();
-	int map[size];
-	memset(map,0,sizeof(map));
+	int map[26] = { 0 };
 	// 比较s1,s2是否包含相同的字符集
 	for (int i = 0; i < size; ++i)
 	{
@@ -66,23 +67,87 @@ bool isScramble(string s1, string s2) {
 		if (map[i] != 0) return false;
 	}
 
+	// 拆分当前子串，以长度为拆分基准，范围为1~size-1
+	// 因为拆分后的子串是可以互换的，因此每个长度存在两种有效的拆分方式：
+	//	1. 0-len-size
+	//	2. 0-(size-len)-size
 	for (int len = 1; len < size; ++len)
 	{
+		// 拆分方式1
 		string s1_left = s1.substr(0,len);
 		string s1_right = s1.substr(len);
-		if (isScramble(s1_left, s2.substr(0,len)) && isScramble(s1_right, s2.substr(len))) 
-			return true;
-		if (isScramble(s1_left, s2_right) && isScramble(s1_right, s2_left)) 
-			return true;
+		string s2_left = s2.substr(0, len);
+		string s2_right = s2.substr(len);
+		if (isScramble(s1_left, s2_left) && isScramble(s1_right, s2_right)) return true;
+		// 拆分方式2
+		s2_left = s2.substr(0, size - len);
+		s2_right = s2.substr(size - len);
+		if (isScramble(s1_left, s2_right) && isScramble(s1_right, s2_left)) return true;
 	}
 	return false;
+}
+
+// 包含26个素数的数组，之所以使用素数是为了表示每一个字母的原子性
+static constexpr int p[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+
+bool isScrambleWithPrimeSet(string s1, string s2) {
+	if (s1.size() != s2.size()) return false;
+	if (s1 == s2) return true;
+	int size = s1.size();
+	long s1_left_hash = p[s1[0]-'a'];
+	long s2_left_hash = p[s2[0]-'a'];
+	long s2_right_hash = p[s2[size-1-0]-'a'];
+	for (int len = 1; len < size; ++len)
+	{
+		// 拆分方式1
+		string s1_left = s1.substr(0,len);
+		string s1_right = s1.substr(len);
+		string s2_left = s2.substr(0, len);
+		string s2_right = s2.substr(len);
+		// 只比较了s1，s2左边的hash值，s1右边的字符集是否与s2右边的字符集匹配则留在下一次递归中检测
+		if (s1_left_hash == s2_left_hash && isScramble(s1_left, s2_left) && isScramble(s1_right, s2_right)) return true;
+		// 拆分方式2
+		s2_left = s2.substr(0, size - len);
+		s2_right = s2.substr(size - len);
+		// 同理比较了半边的字符集
+		if (s1_left_hash == s2_right_hash && isScramble(s1_left, s2_right) && isScramble(s1_right, s2_left)) return true;
+		// 更新hash值
+		s1_left_hash *= p[s1[len]-'a'];
+		s2_left_hash *= p[s2[len]-'a'];
+		s2_right_hash *= p[s2[size-len-1]-'a'];
+	}
+	return false;
+}
+
+bool isScrambleDP(string s1, string s2) {
+	if (s1.size() != s2.size()) return false;
+    if (s1 == s2) return true;
+    int n = s1.size();
+    bool dp[n][n][n+1];
+    memset(dp, false, sizeof(dp));
+    for (int len = 1; len <= n; ++len) {
+        for (int i = 0; i <= n - len; ++i) {
+            for (int j = 0; j <= n - len; ++j) {
+                if (len == 1) {
+                    dp[i][j][1] = s1[i] == s2[j];
+                } else {
+                    for (int k = 1; k < len; ++k) {
+                        if ((dp[i][j][k] && dp[i + k][j + k][len - k]) || (dp[i + k][j][len - k] && dp[i][j + len - k][k])) {
+                            dp[i][j][len] = true;
+                        }
+                    }
+                }                
+            }
+        }
+    }
+    return dp[0][0][n];
 }
 
 int main(int argc, const char* argv[]) {
 	string s1 = "great";
 	string s2 = "rgeat";
 	cout << s1 << " and " << s2 <<" is scramble: ";
-	isScramble(s1, s2) ? (cout << "true") : (cout << "false");
+	isScrambleDP(s1, s2) ? (cout << "true") : (cout << "false");
 	cout << endl;
 	return 0;
 }
