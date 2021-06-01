47bb3231b8c509053ee6514c7c33700b4f67bda4
industriousonesoft <industriousonesoft@gmail.com>
2021-06-01 19:31:41 +0800

    Try 131

---
+++ 131.Palindrome Partitioning/main.cpp
@@ -0,0 +1,41 @@
+/*
+
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+A palindrome string is a string that reads the same backward as forward.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-partitioning
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<string>> partition(string s) {
+
+}
+
+int main(int argc, const char* argv[]) {
+	return 0;
+}
